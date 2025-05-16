6115fa46f2c95061efe1bb186793f40bc3a99c14
mourya jyoti <mouryajyoti657@gmail.com>
2025-05-16 21:37:02 +0530

    add pattern1.js to demonstrate nested loops and string repetition

---
+++ pattern1.js
@@ -0,0 +1,14 @@
+for (i = 0; i <= 5; i++) {
+  let row = "";
+
+  for (j = 0; j <= 5; j++) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+// this another way
+
+for (let i = 0; i < 6; i++) {
+  console.log("*".repeat(3));
+}
