54d5990fdafb7feb6071118dbc0a5f17b548ba57
mourya jyoti <mouryajyoti657@gmail.com>
2025-05-26 12:28:23 +0530

    fibonaccinumber

--- fibonacciNumber.js
+++ fibonacciNumber.js
@@ -3,3 +3,15 @@
 // F(n) = F(n-1) + F(n-2), with seed values F(0) = 0 and F(1) = 1.
 // The Fibonacci sequence starts as follows:
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+//   the arr[i-1]+[i-2]
+
+const fib = (n) => {
+  const arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr[n];
+};
+
+ const result=fib(10);
+ console.log(result)
